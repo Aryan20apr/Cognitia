9275b91f9482234d923ca4a49b47b696e38d1201
Aryan20apr <aryansingh20nov@gmail.com>
2025-09-20 23:41:19 +0530

    Inital commit

---
+++ src/main/java/com/intellidesk/cognitia/CognitiaApplication.java
@@ -0,0 +1,13 @@
+package com.intellidesk.cognitia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intellidesk/cognitia/CognitiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellidesk.cognitia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
