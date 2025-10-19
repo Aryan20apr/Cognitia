30795c51162e66c97e7980e6cbb1c7459e3e3418
Aryan20apr <aryansingh20nov@gmail.com>
2025-10-19 17:33:52 +0530

    Security config, auth controller

--- src/main/java/com/intellidesk/cognitia/userandauth/controllers/UserController.java
+++ src/main/java/com/intellidesk/cognitia/userandauth/controllers/UserController.java
@@ -2,22 +2,24 @@ package com.intellidesk.cognitia.userandauth.controllers;
 
 
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.intellidesk.cognitia.userandauth.models.entities.User;
 import com.intellidesk.cognitia.userandauth.services.impl.UserServiceImpl;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserServiceImpl service;
 
-    public UserController(UserServiceImpl service) {
-        this.service = service;
-    }
 
     @GetMapping
     public List<User> getAll() {
@@ -26,6 +28,7 @@ public class UserController {
 
     @PostMapping
     public User create(@RequestBody User c) {
+        c.setPassword(passwordEncoder.encode(c.getPassword()));
         return service.create(c);
     }
 }
