5a56cf62aa184b41cd4976132dd5501d5ab38b91
Aryan Singh <aryansingh20nov@gmail.com>
2026-01-31 00:27:37 +0530

    feat: implement OrderController for order creation
    - Add OrderController with endpoint to create Razorpay orders
    - Integrate PaymentGateway for order processing
    - Return ApiResponse with order details upon successful creation

--- src/main/java/com/intellidesk/cognitia/payments/controller/PaymentController.java
+++ src/main/java/com/intellidesk/cognitia/payments/controller/PaymentController.java
@@ -1,36 +1,25 @@
 package com.intellidesk.cognitia.payments.controller;
 
-import org.springframework.http.HttpStatus;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.intellidesk.cognitia.ingestion.models.dtos.ApiResponse;
-import com.intellidesk.cognitia.payments.models.dtos.OrderCreationDTO;
-import com.intellidesk.cognitia.payments.models.dtos.OrderDTO;
-import com.intellidesk.cognitia.payments.service.gateway.PaymentGateway;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 
 @RestController
-@RequestMapping("/api/v1/order")
-@Slf4j
-@RequiredArgsConstructor
+@RequestMapping("/api/v1/payment")
 public class PaymentController {
-
-    private final PaymentGateway paymentGateway;
-
-    @PostMapping
-    public ResponseEntity<ApiResponse<?>> createRazorpayOrder(@RequestBody OrderCreationDTO order) {
-        
-        OrderDTO newOrder = paymentGateway.createOrder(order);
-        ApiResponse<OrderDTO> apiResponse = new ApiResponse<>("Order Created Successfully", true, newOrder);
-        return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
-    }
     
+
+    @PostMapping()
+    public ResponseEntity<?> handlePayment(@RequestBody Map<String,Object> payload) {
+        
+       
+        
+        return null;
+    }
     
 }
