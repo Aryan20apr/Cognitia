071053f60794f828252a581f84015242c0cf2094
Aryan Singh <aryansingh20bov@gmail.com>
2026-05-02 12:46:20 +0530

    Migration for all entities to use uuidv7

--- src/main/java/com/intellidesk/cognitia/utils/uuidv7/UuidV7Generator.java
+++ src/main/java/com/intellidesk/cognitia/utils/uuidv7/UuidV7Generator.java
@@ -1,9 +1,9 @@
 package com.intellidesk.cognitia.utils.uuidv7;
 
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.id.enhanced.SequenceStyleGenerator;
+import org.hibernate.id.IdentifierGenerator;
 
-public class UuidV7Generator extends SequenceStyleGenerator {
+public class UuidV7Generator implements IdentifierGenerator {
     @Override
     public Object generate(SharedSessionContractImplementor session, Object object) {
         return UuidUtils.randomV7();
